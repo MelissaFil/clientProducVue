4e217670f45c9cc0fc4f5825288186d9f8e1a315
Melissa <programadorimd@hotmail.com>
2024-03-06 14:30:56 -0300

    melhorando o form para receber produtos quando eu quiser cadastrar cliente

--- cliente-product/src/components/Form.vue
+++ cliente-product/src/components/Form.vue
@@ -1,34 +1,47 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-        <div class="mb-3" v-for="campo in campos" :key="campo">
-            <label :for="campo" class="form-label">{{ campo }}</label>
-            <input type="text" class="form-control" :id="campo" v-model="formulario[campo]" required>
+  <form @submit.prevent="handleSubmit">
+    <div class="mb-3" v-for="campo in campos" :key="campo">
+      <label :for="campo" class="form-label">{{ campo }}</label>
+      <template v-if="campo === 'Produtos'">
+        <div v-for="produto in produtos" :key="produto.id">
+          <input type="checkbox" :id="produto.id" v-model="formulario.produtos" :value="produto.id">
+          <label class="mx-2" :for="produto.id">{{ produto.nome }}</label>
         </div>
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
-    </form>
+      </template>
+      <template v-else>
+        <input type="text" class="form-control" :id="campo" v-model="formulario[campo]" required>
+      </template>
+    </div>
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
+  </form>
 </template>
 
 <script>
 export default {
-    name: "Form",
-    props: {
-        campos: Array
+  name: "Form",
+  props: {
+    campos: Array,
+    produtos: Array
+  },
+  data() {
+    return {
+      formulario: {
+        produtos: [] 
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("cadastrar", this.formulario);
+      this.limparFormulario();
     },
-    data() {
-        return {
-            formulario: {}
-        };
-    },
-    methods: {
-        handleSubmit() {
-            this.$emit("cadastrar", this.formulario);
-            this.limparFormulario();
-        },
-        limparFormulario() {
-            for (let campo of this.campos) {
-                this.formulario[campo] = "";
-            }
-        }
+    limparFormulario() {
+      for (let campo of this.campos) {
+        this.formulario[campo] = "";
+      }
+      this.formulario.produtos = [];
     }
+  }
 };
 </script>
+
