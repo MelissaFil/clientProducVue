58bf721c2c5bce5691911c8eafa0726468e25f91
Melissa <programadorimd@hotmail.com>
2024-03-06 14:06:16 -0300

    componentizando form

--- cliente-product/src/components/Form.vue
+++ cliente-product/src/components/Form.vue
@@ -1,17 +1,34 @@
 <template>
-    <form>
-        <div class="mb-3">
-            <label for="" class="form-label"></label>
-            <input type="" class="form-control" id="" >
+    <form @submit.prevent="handleSubmit">
+        <div class="mb-3" v-for="campo in campos" :key="campo">
+            <label :for="campo" class="form-label">{{ campo }}</label>
+            <input type="text" class="form-control" :id="campo" v-model="formulario[campo]" required>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
     </form>
 </template>
+
 <script>
-    export default{
-        name: "Form",
-        props:{
-            campos:Array,
+export default {
+    name: "Form",
+    props: {
+        campos: Array
+    },
+    data() {
+        return {
+            formulario: {}
+        };
+    },
+    methods: {
+        handleSubmit() {
+            this.$emit("cadastrar", this.formulario);
+            this.limparFormulario();
+        },
+        limparFormulario() {
+            for (let campo of this.campos) {
+                this.formulario[campo] = "";
+            }
         }
     }
+};
 </script>
