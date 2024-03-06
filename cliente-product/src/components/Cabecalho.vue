f5182942876f3959c16fefd6feaebed7b24c8236
Melissa <programadorimd@hotmail.com>
2024-03-06 11:28:07 -0300

    criando componente cabecalho

---
+++ cliente-product/src/components/Cabecalho.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container my-5">
+        <div class="d-flex flex-row align-items-center justify-content-between">
+            <h4>{{ nomePagina }}</h4>
+            <router-link to="/clientes/cadastro" class="btn btn-primary">Cadastrar {{ nomePagina }}</router-link>
+        </div> 
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Cabecalho",
+        props:{
+            nomePagina: String
+        }
+        
+    }
+
+</script>
--- cliente-product/src/views/Clientes.vue
+++ cliente-product/src/views/Clientes.vue
@@ -1,3 +1,17 @@
 <template>
-    
+    <Cabecalho :nome-pagina="pagina"/>
 </template>
+<script>
+    import Cabecalho from "../components/Cabecalho.vue";
+    export default{
+        name:'Clientes',
+        data(){
+            return{
+                pagina: 'Clientes'
+            }
+        },
+        components:{
+            Cabecalho
+        }
+    }
+</script>
