14f1ac23020a7744a6e656d3cb5ac932fd12d894
Melissa <programadorimd@hotmail.com>
2024-03-06 11:41:27 -0300

    corrigindo link do button criar

--- cliente-product/src/components/Cabecalho.vue
+++ cliente-product/src/components/Cabecalho.vue
@@ -2,7 +2,7 @@
     <div class="container my-5">
         <div class="d-flex flex-row align-items-center justify-content-between">
             <h4>{{ nomePagina }}</h4>
-            <router-link to="/clientes/cadastro" class="btn btn-primary">Cadastrar {{ nomePagina }}</router-link>
+            <router-link :to="`/${nomePagina}/cadastro`" class="btn btn-primary">Cadastrar {{ nomePagina }}</router-link>
         </div> 
     </div>
 </template>
