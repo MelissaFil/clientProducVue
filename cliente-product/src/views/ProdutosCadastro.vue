7985dff6560f27d1105208f771d78ebb80fff8d1
Melissa <programadorimd@hotmail.com>
2024-03-06 14:06:39 -0300

    criando cadastro de produtos

--- cliente-product/src/views/ProdutosCadastro.vue
+++ cliente-product/src/views/ProdutosCadastro.vue
@@ -1,27 +1,41 @@
-<template>
+<<template>
     <div class="container">
-    <Cabecalho :tipo="'cadastro'" :nome-pagina="pagina"/>
+        <Cabecalho :tipo="'cadastro'" :nome-pagina="pagina"/>
+        <Form :campos="campos" @cadastrar="createProduto" />
     </div>
-
 </template>
-<script>
-    import Cabecalho from "../components/Cabecalho.vue";
-    export default{
-        name:'ProdutosCadastro',
-        data(){
-            return{
-                pagina: 'Cadastro de produtos',
-                campo: ['Nome'],
-            }
-        },
-        components:{
-            Cabecalho,
 
-        },
-        methods:{
-        },
-        mounted(){
-        
+<script>
+import Cabecalho from "../components/Cabecalho.vue";
+import Form from "../components/Form.vue";
+
+export default {
+    name:'ProdutosCadastro',
+    data(){
+        return{
+            pagina: 'Cadastro de produtos',
+            campos: ['Nome'],
+        }
+    },
+    components:{
+        Cabecalho,
+        Form
+    },
+    methods:{
+        async createProduto(dados){
+            const data = {
+                nome: dados.Nome,
+                ativo: true
+            };
+
+            const dataJson = JSON.stringify(data);
+
+            await fetch("http://localhost:3000/produtos", {
+                method: "POST",
+                headers: { "Content-Type" : "application/json" },
+                body: dataJson
+            });
         }
     }
+};
 </script>
